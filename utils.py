5c747d7a39c9af5692905fad68e1feb83d1a0ab3
Diego Kiedanski <dkiedanski@gmail.com>
2022-01-17 00:07:21 +0000

    update utils

--- utils.py
+++ utils.py
@@ -110,3 +110,28 @@ def build_exercise(username, config, url="https://notebook-grading.herokuapp.com
         return r.text
     
     return user_data
+
+
+def check_solution(proposal, url="https://notebook-grading.herokuapp.com"):
+    import requests
+    r = requests.post(
+        url + "/attempt/",
+        json = proposal,
+    )
+    if r.status_code != 200:
+        return r.text
+    res = r.json() 
+
+    max_len = 50
+    answers = res["task_attempts"]
+    title = f"Total Correct Answers {res['total_correct']} / {len(answers)}"
+    row = f"|{{0:^{max_len}}}|{{1:^20}}|"
+    sep = row.format("-" * max_len, "-" * 20)
+    print(row.format("Task Name", "Status"))
+    print(sep)
+    print(sep)
+    for a in answers:
+        print(row.format(
+            a["name"][:max_len], 
+            "Correct" if a["is_correct"] else "Incorrect"))
+        print(sep)
